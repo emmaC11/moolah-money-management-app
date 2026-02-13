9173cc5bf9e93452c454f16d51b7ed4694aaed47
Cyril Mannion <cyrilmannion@gmail.com>
2026-02-13 19:24:44 +0000

    Add Google sign-in and Firebase config fixes

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,7 +1,7 @@
+var admin = require("firebase-admin");
 
-const admin = require('firebase-admin');
-// Use GOOGLE_APPLICATION_CREDENTIALS env var or load a service account JSON explicitly.
-if (!admin.apps.length) {
-  admin.initializeApp({ credential: admin.credential.applicationDefault() });
-}
-module.exports = admin;
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
